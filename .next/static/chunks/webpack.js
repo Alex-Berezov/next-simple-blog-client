4d6be18fdd7c9d89172c6640ce2fb85ff6e633a8
Aleksey Berezov <a.berezov@leningrad.io>
2022-08-02 10:35:13 +0400

    pre-commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a2b8327d7604ac3b"; }
+/******/ 		__webpack_require__.h = function() { return "9f9d4b38947b3028"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
